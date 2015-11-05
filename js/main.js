5855fcf88191ebf83b50991c2297ceebff3fe021
dis-zephyro <zephyro@yandex.ru>
2015-11-05 05:09:18 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+///Reply Slider
+
+$('.reply').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 5200,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+$('.reply__nav.prev').click(function(){
+    $('.reply').slick('slickPrev');
+});
+
+$('.reply__nav.next').click(function(){
+    $('.reply').slick('slickNext');
+});
