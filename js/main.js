f06e730e0cb4255045875085df4fd4b0674d960b
dis-zephyro <zephyro@yandex.ru>
2015-11-05 23:58:56 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -24,3 +24,35 @@ $('.reply__nav.next').click(function(){
 $(".btn-modal").fancybox({
     'padding'    : 0
 });
+
+
+$('.btn-compare input[type="radio"]').click(function() {
+    var box = $(this).closest('.container');
+    var check1 = ($('#compare01').is(':checked'));
+    var check2 = ($('#compare02').is(':checked'));
+
+    if (check1) {
+        box.find('.comparison__rest').hide();
+        box.find('.comparison__fitt').show();
+    }
+
+    if (check2) {
+        box.find('.comparison__fitt').hide();
+        box.find('.comparison__rest').show();
+    }
+});
+
+
+
+$('input[type="checkbox"]').click(function() {
+    var rows = $(this).closest('tr');
+    var check = rows.find('span.check i');
+    var checkbox = ($(this).is(':checked'));
+
+    if (checkbox) {
+        check.addClass('text-success');
+    }
+    else{
+        check.removeClass('text-success');
+    }
+});
