6caa56bd488d398e052ccddaf5f7c53b9557ae2e
dis-zephyro <zephyro@yandex.ru>
2015-11-07 01:20:51 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
 $(window).on('load', function () {
     var $preloader = $('.preloader-inner');
-    $preloader.delay(2500).fadeOut('slow');
+    $preloader.delay(1800).fadeOut('slow');
 });
 
 ///Reply Slider
